f51cfc113e42630b866ee5d8393702e0e002a226
asantinuredin739-ai <asantinuredin739@gmail.com>
2026-06-15 10:22:18 +0300

    Create Flipping an image.py

---
+++ Flipping an image.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for row in image:
+            row.reverse()
+
+            for i in range(len(row)):
+                row[i] = 1 - row[i]
+
+        return image
