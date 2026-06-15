f9ef26d42df47bd1a71d4598cea783c8240a0f79
asantinuredin739-ai <asantinuredin739@gmail.com>
2026-06-15 10:38:20 +0300

    Create Island perimeter.py

---
+++ Island perimeter.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        perimeter = 0
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimeter += 4
+
+                    # Check upper neighbor
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+
+                    # Check left neighbor
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
