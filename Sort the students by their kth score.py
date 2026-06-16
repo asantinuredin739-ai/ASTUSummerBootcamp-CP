4c4b92a6b3facf8787cd2e8cd2fb91272665a68c
asantinuredin739-ai <asantinuredin739@gmail.com>
2026-06-16 07:58:38 +0300

    Create Sort the students by their kth score.py

---
+++ Sort the students by their kth score.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sortTheStudents(self, score, k):
+        """
+        :type score: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        score.sort(key=lambda row: row[k], reverse=True)
+        return score
