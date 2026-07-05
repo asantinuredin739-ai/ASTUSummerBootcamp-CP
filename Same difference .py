5fa8e7965e1de2e9ae07c18646a3ce5f934eb628
asantinuredin739-ai <asantinuredin739@gmail.com>
2026-07-05 07:13:56 +0300

    Create Same difference .py

---
+++ Same difference .py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    last = s[-1]
+    ans = 0
+
+    for i in range(n - 1):
+        if s[i] != last:
+            ans += 1
+
+    print(ans)
